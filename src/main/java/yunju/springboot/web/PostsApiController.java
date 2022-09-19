12d3b4c4883da1fd3ffe1cd1cb941556a1129478
Yunju Kang <spirit331@naver.com>
2022-09-19 23:37:31 +0900

    게시글 등록 화면 구현

---
+++ src/main/java/yunju/springboot/web/PostsApiController.java
@@ -0,0 +1,36 @@
+package yunju.springboot.web;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import yunju.springboot.service.posts.PostsService;
+import yunju.springboot.web.dto.PostsResponseDto;
+import yunju.springboot.web.dto.PostsSaveRequestDto;
+import yunju.springboot.web.dto.PostsUpdateRequestDto;
+
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+    return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+
+}
