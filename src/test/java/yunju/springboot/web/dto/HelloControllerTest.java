e04cb946329e0f9c1617847f2eb075e13148d259
Yunju Kang <spirit331@naver.com>
2022-09-29 00:21:58 +0900

    HelloControllerTest 오류 수정

--- src/test/java/yunju/springboot/web/dto/HelloControllerTest.java
+++ src/test/java/yunju/springboot/web/dto/HelloControllerTest.java
@@ -1,5 +1,5 @@
-package yunju.springboot;
-import org.junit.jupiter.api.Test;
+package yunju.springboot.web.dto;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,40 +13,43 @@ import yunju.springboot.web.HelloController;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
+
     @Autowired
     private MockMvc mvc;
 
-    @WithMockUser(roles = "USER")
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().string(hello));
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
-    @WithMockUser(roles = "USER")
+
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(
-                get("/hello/dto")
-                        .param("name", name)
-                        .param("amount", String.
-                                valueOf(amount)))
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
     }
-
-
-
 }
