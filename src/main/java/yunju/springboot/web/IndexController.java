4d305a93d2b63b6d9b3c711fd1ec80fb62dcf9f6
Yunju Kang <spirit331@naver.com>
2022-09-28 21:22:10 +0900

    구글 로그인, 네이버 로그인 추가

--- src/main/java/yunju/springboot/web/IndexController.java
+++ src/main/java/yunju/springboot/web/IndexController.java
@@ -5,16 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import yunju.springboot.config.auth.LoginUser;
+import yunju.springboot.config.auth.dto.SessionUser;
 import yunju.springboot.service.posts.PostsService;
 import yunju.springboot.web.dto.PostsResponseDto;
 
-import java.text.BreakIterator;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("myName", user.getName());
+        }
+        return "index";
+    }
+
 
     @GetMapping("/posts/save")
     public String postsSave() {
@@ -22,16 +33,11 @@ public class IndexController {
     }
 
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        return "index";
-    }
-
     @GetMapping("/posts/update/{id}")
     public String postUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
         return "posts-update";
     }
+
 }
--- src/test/java/yunju/springboot/HelloControllerTest.java
+++ src/test/java/yunju/springboot/HelloControllerTest.java
@@ -3,8 +3,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import yunju.springboot.config.auth.SecurityConfig;
 import yunju.springboot.web.HelloController;
 
 import static org.hamcrest.Matchers.is;
@@ -12,11 +16,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -26,7 +31,7 @@ public class HelloControllerTest {
                         status().isOk(),
                         content().string(hello));
     }
-
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
